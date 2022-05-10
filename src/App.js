bbf47de488a6996cd3e282af77c2431ccdce1a64
Jorge Gimenez <jorgemg0908@gmail.com>
2022-05-10 12:12:40 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,54 +1,62 @@
 import React from "react";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import { Icon } from "leaflet";
-import * as parkData from "./data/skateboard-parks.json";
-import "./app.css";
+import '../src/App.css'
+import axios from 'axios';
 
-export const icon = new Icon({
-  iconUrl: "/skateboarding.svg",
-  iconSize: [25, 25]
-});
-
-export default function App() {
-  const [activePark, setActivePark] = React.useState(null);
-
-  return (
-    <Map center={[45.4, -75.7]} zoom={12}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-
-      {parkData.features.map(park => (
-        <Marker
-          key={park.properties.PARK_ID}
-          position={[
-            park.geometry.coordinates[1],
-            park.geometry.coordinates[0]
-          ]}
-          onClick={() => {
-            setActivePark(park);
-          }}
-          icon={icon}
-        />
-      ))}
-
-      {activePark && (
-        <Popup
-          position={[
-            activePark.geometry.coordinates[1],
-            activePark.geometry.coordinates[0]
-          ]}
-          onClose={() => {
-            setActivePark(null);
-          }}
-        >
-          <div>
-            <h2>{activePark.properties.NAME}</h2>
-            <p>{activePark.properties.DESCRIPTIO}</p>
+export default class PersonList extends React.Component {
+  state = {
+   ubicaciones: []
+  }
+  componentDidMount() {
+    axios.get(`http://172.30.15.5:3015/api/mapaapi`)
+      .then(res => {
+        const ubicaciones = res.data;
+        this.setState({ ubicaciones });})  
+  }
+icon = new Icon({
+   iconUrl: "https://cdn-icons-png.flaticon.com/512/6395/6395324.png",
+   iconSize: [23, 23]
+ });
+render(){ 
+    console.log(this.state.ubicaciones.length)
+    if (this.state.ubicaciones.length != 0 && this.state.ubicaciones.length != undefined  ) {
+      return (
+      
+        <div id="MapContainer">
+          
+        <Map center={[-27.380384, -55.918672]} zoom={14}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/> 
+          
+          {this.state.ubicaciones.map (person =>
+            <Marker position={[person.latitud, person.longitud]} icon={this.icon}>
+            <Popup>{person.descripcionEstadoVehiculo} 
+              <p>Linea:{person.abreviaturaLinea}</p>
+              <p>Chofer: {person.apellidoChofer} {person.nombreChofer}</p>
+              <p>Codigo de Equipo: {person.codigoEquipo}</p>
+              </Popup>      
+           </Marker>)}
+          </Map> 
           </div>
-        </Popup>
-      )}
-    </Map>
-  );
+          
+        );
+    }else{
+      return (
+      
+        <div id="MapContainer">
+          
+        <Map center={[-27.380384, -55.918672]} zoom={14}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/> 
+          </Map> 
+          </div>
+          
+        );
+    }
+    
+
 }
+}
+setInterval('window.location.reload()' ,10000);
+
